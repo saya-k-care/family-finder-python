592e65156660f9a56c36da171dce157adf38c62d
pcyuen98 <pcyuen98@gmail.com>
2024-07-29 15:16:02 +0800

    updates

--- src/schedule.py
+++ src/schedule.py
@@ -4,10 +4,14 @@ import subprocess
 import argparse
 import sys
 
-path = None
+filetorun = None
 
 def your_job(filetorun):
         logging.info("executing " + "test")
+        subprocess.Popen('python3 ' + filetorun, shell=True)
+
+def your_job2():
+        logging.info("executing " + "test" + filetorun)
         subprocess.Popen('python3 ' + filetorun, shell=True)
 
 if __name__ == '__main__':
@@ -19,7 +23,7 @@ if __name__ == '__main__':
     filetorun = sys.argv[1]
     print("\nMinutes:", sys.argv[2])
     minute = int(sys.argv[2])
-    logging.basicConfig(filename='schedule',
+    logging.basicConfig(filename='schedule.txt',
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
@@ -29,6 +33,6 @@ if __name__ == '__main__':
 
     logging.info("Starting scheduler")
     scheduler = BlockingScheduler()
-    scheduler.add_job(your_job(filetorun), 'interval', minutes=minute)
+    scheduler.add_job(your_job2, 'interval', minutes=minute)
     scheduler.start()
 
